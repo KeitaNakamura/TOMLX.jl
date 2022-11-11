b1e6dd1536736ff80cd3a4a4f541911cbaa6353c
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-11-11 17:10:18 +0900

    Set version to 0.2.3 (#3)
    * Set version to 0.2.3
    
    * Add more tests

--- test/parse.jl
+++ test/parse.jl
@@ -1,19 +1,20 @@
 @testset "parse" begin
+    parse(str) = TOMLX.parse(@__MODULE__, str)
     @testset "string" begin
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             str = "hi"
-        """)
+        """ |> parse
         @test dictx[:str] == "hi"
     end
     @testset "function" begin
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             func = x -> 2x^2
-        """)
+        """ |> parse
         @test dictx[:func](3) == 18
     end
     @testset "special characters" begin
         # inf and nan are allowed in julia expression
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             x1 = π
             x2 = inf
             x3 = Inf
@@ -25,7 +26,7 @@
             x9 = NaN
             x10 = [π,inf,Inf,+inf,+Inf,-inf,-Inf,nan,NaN]                # wrapped case
             x11 = map(identity, (π,inf,Inf,+inf,+Inf,-inf,-Inf,nan,NaN)) # complex expression
-        """)
+        """ |> parse
         @test dictx[:x1] === π
         @test dictx[:x2] === Inf
         @test dictx[:x3] === Inf
@@ -39,27 +40,37 @@
         @test all(dictx[:x11] .=== (π,Inf,Inf,+Inf,+Inf,-Inf,-Inf,NaN,NaN))
     end
     @testset "calculation" begin
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             x1 = π * 3
             x2 = 2.0 * 3
             x3 = (1,2,3) .* 2.0
-        """)
+        """ |> parse
         @test dictx[:x1] === π * 3
         @test dictx[:x2] === 2.0 * 3
         @test dictx[:x3] === (1,2,3) .* 2.0
     end
     @testset "external package" begin
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             x1 = SVector(1,2)
-        """)
+        """ |> parse
         @test dictx[:x1] === SVector(1,2)
     end
     @testset "inner table" begin
-        dictx = TOMLX.parse(@__MODULE__, """
+        dictx = """
             tables = [{x=SVector(1.0,2.0), y=SVector(3.0,4.0)}, {x=SVector(5.0,6.0), y=SVector(7.0,8.0)}]
-        """)
+        """ |> parse
         @test dictx[:tables] == [Dict{Symbol,Any}(:x=>SVector(1.0,2.0), :y=>SVector(3.0,4.0)),
                                  Dict{Symbol,Any}(:x=>SVector(5.0,6.0), :y=>SVector(7.0,8.0)),]
+    end
+    @testset "dots" begin
+        dictx = """
+            a.b = 1
+            a.c = "hello"
+            a.d = π
+        """ |> parse
+        @test dictx[:a][:b] === 1
+        @test dictx[:a][:c] == "hello"
+        @test dictx[:a][:d] === π
     end
 end
 
